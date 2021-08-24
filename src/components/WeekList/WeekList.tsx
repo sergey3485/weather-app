2bcbc15eb0c3812195a0c3473035e835d9a203f8
sergey3485 <33asasergey33@gmail.com>
2021-08-24 15:46:50 +0300

    feat: add scrollbar

--- src/components/WeekList/WeekList.tsx
+++ src/components/WeekList/WeekList.tsx
@@ -11,17 +11,15 @@ export const WeekList = (props: WeekListProps): JSX.Element => {
   const { hourly, iconPath, timeCode } = props;
   const time = getCurrentHour(timeCode);
   return (
-    <div className="week-container">
-      <div className="day-container">
-        <div className="day">
-          <span>{time.value}</span>
-          <span className="time-index">{` ${time.index}`}</span>
-        </div>
-        <div className="day-icon">
-          <img src={iconPath} alt="weather" className="icon" />
-        </div>
-        <div className="day-temperature">{hourly ?? ''}</div>
+    <div className="day-container">
+      <div className="day">
+        <span>{time.value}</span>
+        <span className="time-index">{` ${time.index}`}</span>
       </div>
+      <div className="day-icon">
+        <img src={iconPath} alt="weather" className="icon" />
+      </div>
+      <div className="day-temperature">{hourly ?? ''}</div>
     </div>
   );
 };
